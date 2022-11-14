1dd8c75efd67632964379c0708d5db8ef41b4633
Do Trinh <contact@dotrinh.com>
2022-11-14 17:50:44 +0900

    [KNOB] left img & right text (showcase)

--- app/src/main/java/com/dotrinh/canvas/draw_arc/draw_knob/DrawKnobActivity.java
+++ app/src/main/java/com/dotrinh/canvas/draw_arc/draw_knob/DrawKnobActivity.java
@@ -5,13 +5,16 @@
 
 package com.dotrinh.canvas.draw_arc.draw_knob;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.util.DisplayMetrics;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.dotrinh.canvas.R;
+import com.dotrinh.canvas.tool.StringTool;
 
 public class DrawKnobActivity extends AppCompatActivity {
 
@@ -22,8 +25,42 @@ public class DrawKnobActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_knob);
         ctx = this;
-        MyKnob myKnob = findViewById(R.id.knob);
-        myKnob.center_bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.verified);
-        myKnob.left_bmp = BitmapFactory.decodeResource(getResources(),R.drawable.editampnote);
+        MyKnob myKnob = findViewById(R.id.knob_1);
+        myKnob.relative_size = (int) convertDpToPx(70);
+        // myKnob.center_bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.verified);
+        // myKnob.left_bmp = BitmapFactory.decodeResource(getResources(), R.drawable.editampnote);
+        myKnob.current_content = MyKnob.CONTENT_TYPE.text_only;
+        // // myKnob.current_content = MyKnob.CONTENT_TYPE.img_only;
+        // // myKnob.current_content = MyKnob.CONTENT_TYPE.img_and_text;
+        if (myKnob.current_content == MyKnob.CONTENT_TYPE.text_only) {
+            float txtSize = getFitTextSizeHorizontal(myKnob.center_text_paint, myKnob.relative_size, "4000"); //calculate text font size
+            myKnob.center_text_paint.setTextSize(txtSize); //calculate text font size
+        } else if (myKnob.current_content == MyKnob.CONTENT_TYPE.img_only) {
+
+        } else if (myKnob.current_content == MyKnob.CONTENT_TYPE.img_and_text) {
+        }
+
+        MyKnob myKnob2 = findViewById(R.id.knob_2);
+        myKnob2.relative_size = (int) convertDpToPx(70);
+        myKnob2.center_bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.verified);
+        myKnob2.current_content = MyKnob.CONTENT_TYPE.img_only;
+
+
+        MyKnob myKnob3 = findViewById(R.id.knob_3);
+        myKnob3.relative_size = (int) convertDpToPx(70);
+        myKnob3.left_bmp = BitmapFactory.decodeResource(getResources(), R.drawable.editampnote);
+        myKnob3.current_content = MyKnob.CONTENT_TYPE.img_and_text;
+
     }
+
+    public static float getFitTextSizeHorizontal(TextPaint currentPaint, float newWidth, String currentText) {
+        float oldWidth = StringTool.getTextBoundWidthOfString(currentText, currentPaint); //calculate more accurately, dont use measure text at here
+        float factor = (newWidth / oldWidth);
+        return currentPaint.getTextSize() * factor;
+    }
+
+    public float convertDpToPx(float dp) {
+        return dp * ((float) getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
 }
--- app/src/main/java/com/dotrinh/canvas/draw_arc/draw_knob/MyKnob.java
+++ app/src/main/java/com/dotrinh/canvas/draw_arc/draw_knob/MyKnob.java
@@ -25,16 +25,23 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.dotrinh.canvas.R;
+import com.dotrinh.canvas.tool.StringTool;
 import com.dotrinh.canvas.tool.Tool;
 
 public class MyKnob extends View implements View.OnTouchListener {
 
-    TextPaint textPaint;
-    Paint myPaint;
+    Paint progress_realtime_paint;
     Paint BG_paint;
     Paint circle_paint;
+    private Paint line;
 
-    RectF testRect;
+    public enum CONTENT_TYPE {
+        text_only,
+        img_only,
+        img_and_text
+    }
+
+    public CONTENT_TYPE current_content = CONTENT_TYPE.text_only;
 
     public MyKnob(Context context) {
         super(context);
@@ -54,18 +61,27 @@ public class MyKnob extends View implements View.OnTouchListener {
     private void initialize() {
         setOnTouchListener(this);
 
-        textPaint = new TextPaint();
-        textPaint.setTypeface(Typeface.SERIF);
-        textPaint.setStrokeWidth(7);
-        textPaint.setTextSize(Tool.convertSpToPx(getContext(), 40));
-        textPaint.setAntiAlias(true);
-        textPaint.setPathEffect(null);
-        textPaint.setColor(Color.BLUE);
-        textPaint.setStyle(Paint.Style.STROKE);
+        center_text_paint = new TextPaint();
+        center_text_paint.setTypeface(Typeface.SERIF);
+        center_text_paint.setStrokeWidth(7);
+        center_text_paint.setTextSize(40);
+        center_text_paint.setAntiAlias(true);
+        center_text_paint.setPathEffect(null);
+        center_text_paint.setColor(Color.BLUE);
+        center_text_paint.setStyle(Paint.Style.STROKE);
 
-        myPaint = new Paint();
-        myPaint.setColor(getResources().getColor(R.color.white, null));
-        myPaint.setAntiAlias(true);
+        right_text_paint = new TextPaint();
+        right_text_paint.setTypeface(Typeface.SERIF);
+        right_text_paint.setStrokeWidth(7);
+        right_text_paint.setTextSize(Tool.convertSpToPx(getContext(), 40));
+        right_text_paint.setAntiAlias(true);
+        right_text_paint.setPathEffect(null);
+        right_text_paint.setColor(Color.BLUE);
+        right_text_paint.setStyle(Paint.Style.STROKE);
+
+        progress_realtime_paint = new Paint();
+        progress_realtime_paint.setColor(getResources().getColor(R.color.white, null));
+        progress_realtime_paint.setAntiAlias(true);
 
         BG_paint = new Paint();
         BG_paint.setStrokeWidth(80);
@@ -83,81 +99,130 @@ public class MyKnob extends View implements View.OnTouchListener {
         circle_paint.setAntiAlias(true);
 
         // img shape paint
-        imgShapePaint = new Paint();
-        imgShapePaint.setAntiAlias(true);
-        imgShapePaint.setColor(Color.BLACK);
+        img_shape_paint = new Paint();
+        img_shape_paint.setAntiAlias(true);
+        img_shape_paint.setColor(Color.MAGENTA);
 
+        //line
+        line = new Paint();
+        line.setTypeface(Typeface.SERIF);
+        line.setStrokeWidth(2);
+        line.setAntiAlias(true);
+        line.setPathEffect(null);
+        line.setColor(Color.BLUE);
+        line.setStyle(Paint.Style.STROKE);
     }
 
-    int size = 400;
-    final int radius = size - 50;
-    float degree = 0;
+    public int relative_size;
+    public RectF rect_outer;
+    public int radius;
+    public float degree = 0;
 
-    //single txt or img
-    String center_string = "x3";
-    Paint imgShapePaint;
+    //-----------------center single text
+    TextPaint center_text_paint;
+    public String center_string = "4000";
+
+    //-----------------center single text
+    public Paint img_shape_paint;
     public Bitmap center_bitmap;
-    public Rect bitmap_rect = new Rect(0, 0, 200, 200);
+    public Rect center_bmp_rect;
 
-    //txt & img group
+    //-----------------center image & text group
+    public Rect bmp_txt_grp_rect;
+    public int x_Grp;
+    public int y_Grp;
+
     public Bitmap left_bmp;
-    public Rect left_bmp_rect = new Rect();
-    public Rect right_txt_rect = new Rect();
-    public Rect bmp_txt_grp_rect = new Rect(0, 0, (int) (radius * 1.55f), radius);
+    public Rect left_bmp_rect;
+
+    public TextPaint right_text_paint;
+    public Rect right_txt_rect;
+    public float yTxt_2;
+
 
     @Override
     protected void onSizeChanged(int newWidth, int newHeight, int xOld, int yOld) {
         super.onSizeChanged(newWidth, newHeight, xOld, yOld);
-        int left = getWidth() / 2 - size;
-        int top = getHeight() / 2 - size;
-        testRect = new RectF(left, top, getWidth() / 2f + size, getHeight() / 2f + size);
-        dx_dy.right = (int) (0.7 * max_val);
+        int left = getWidth() / 2 - relative_size;
+        int right = getWidth() / 2 + relative_size;
+        int top = getHeight() / 2 - relative_size;
+        int bottom = getHeight() / 2 + relative_size;
+        rect_outer = new RectF(left, top, right, bottom); //hình vuông ngoài cùng
+        radius = relative_size - 40;
+
+        //center img case
+        center_bmp_rect = new Rect(0, 0, (int) (relative_size/1.2), (int) (relative_size/1.2));
+
+        //center img & txt case
+        //-----------------center image & text GROUP
+        bmp_txt_grp_rect = new Rect(0, 0, (int) (radius * 1.55f), radius);
+
+
+        // show default knob val
+        dx_dy.right = (int) (0.99 * max_val);
         calculate_degree();
     }
 
-    @SuppressLint("DrawAllocation")
+    boolean isFirst = true;
+
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawRect(0, 0, getWidth(), getHeight(), BG_paint); //full fill
-        canvas.drawRect(testRect, textPaint);
-        canvas.drawArc(testRect, 135, degree, true, myPaint);//realtime
-        canvas.drawCircle(testRect.centerX(), testRect.centerY(), radius, circle_paint); //big circle layer
-        // canvas.drawCircle(testRect.centerX(), testRect.centerY(), 15, textPaint); //test dot
+        canvas.drawRect(rect_outer, center_text_paint);
+        canvas.drawArc(rect_outer, 135, degree, true, progress_realtime_paint);//realtime changes
+        canvas.drawCircle(rect_outer.centerX(), rect_outer.centerY(), radius, circle_paint); //big circle layer
+        // canvas.drawCircle(rect_outer.centerX(), rect_outer.centerY(), 15, center_text_paint); //test dot
 
-        //-----------------center text
-        // float xTxt = testRect.centerX() - getTextBoundWidthOfString(center_string, textPaint) / 2f;
-        // float yTxt = (testRect.top + (testRect.height() / 2f + getTextBoundHeightWithBottom(center_string, textPaint) / 2f));
-        // canvas.drawText(center_string, xTxt, yTxt, textPaint);
+        //-----------------center single text case
+        if(current_content == CONTENT_TYPE.text_only) {
+            float xTxt = rect_outer.centerX() - StringTool.getTextBoundWidthOfString(center_string, center_text_paint) / 2f;
+            float yTxt = (rect_outer.top + (rect_outer.height() / 2f + getTextBoundHeightWithBottom(center_string, center_text_paint) / 2f));
+            canvas.drawText(center_string, xTxt, yTxt, center_text_paint);
+        }
 
-        //-----------------center image
-        // float xBmp = testRect.centerX() - bitmap_rect.width() / 2f;
-        // float yBmp = testRect.centerY() - bitmap_rect.height() / 2f;
-        // bitmap_rect = new Rect((int) xBmp, (int) yBmp, (int) (xBmp + bitmap_rect.width()), (int) (yBmp + bitmap_rect.height()));
-        // canvas.drawRect(bitmap_rect, textPaint);
-        // canvas.drawBitmap(center_bitmap, new Rect(0, 0, center_bitmap.getWidth(), center_bitmap.getHeight()), bitmap_rect, imgShapePaint);
+        //-----------------center single image case
+        if(current_content == CONTENT_TYPE.img_only) {
+            float xBmp = rect_outer.centerX() - center_bmp_rect.width() / 2f;
+            float yBmp = rect_outer.centerY() - center_bmp_rect.height() / 2f;
+            center_bmp_rect = new Rect((int) xBmp, (int) yBmp, (int) (xBmp + center_bmp_rect.width()), (int) (yBmp + center_bmp_rect.height()));
+            canvas.drawRect(center_bmp_rect, center_text_paint);
+            canvas.drawBitmap(center_bitmap, new Rect(0, 0, center_bitmap.getWidth(), center_bitmap.getHeight()), center_bmp_rect, img_shape_paint);
+        }
 
-        //-----------------center image & text group
-        float xGrp = testRect.centerX() - bmp_txt_grp_rect.width() / 2f;
-        float yGrp = testRect.centerY() - bmp_txt_grp_rect.height() / 2f;
-        bmp_txt_grp_rect = new Rect((int) xGrp, (int) yGrp, (int) (xGrp + bmp_txt_grp_rect.width()), (int) (yGrp + bmp_txt_grp_rect.height()));
-        // canvas.drawRect(bmp_txt_grp_rect, textPaint);
-            // textPaint.setTextSize(bmp_txt_grp_rect.width()/4f);
+        //-----------------left image & right text GROUP case
+        if(current_content == CONTENT_TYPE.img_and_text) {
+            x_Grp = (int) (rect_outer.centerX() - bmp_txt_grp_rect.width() / 2f);
+            y_Grp = (int) (rect_outer.centerY() - bmp_txt_grp_rect.height() / 2f);
+            int w_Grp = x_Grp + bmp_txt_grp_rect.width();
+            int h_Grp = y_Grp + bmp_txt_grp_rect.height();
+            bmp_txt_grp_rect = new Rect(x_Grp, y_Grp, w_Grp, h_Grp);//update right, bottom
+            canvas.drawRect(bmp_txt_grp_rect, center_text_paint);
 
-            //right bmp
-            int yVertical_bmp = (int) (bmp_txt_grp_rect.top + (bmp_txt_grp_rect.height() / 2f - left_bmp.getHeight() / 2f)); //draw from top - left
-            left_bmp_rect = new Rect((int) xGrp, yVertical_bmp, (int) (xGrp + left_bmp.getWidth()), (int) (yVertical_bmp + left_bmp.getHeight()));
-            // canvas.drawRect(left_bmp_rect, imgShapePaint);//black bg
-            canvas.drawBitmap(left_bmp, new Rect(0, 0, left_bmp.getWidth(), left_bmp.getHeight()), left_bmp_rect, imgShapePaint);
+            canvas.drawLine(bmp_txt_grp_rect.centerX(), bmp_txt_grp_rect.top, bmp_txt_grp_rect.centerX(), bmp_txt_grp_rect.bottom, line); //center line
 
-            //left text
-            float yTxt_2 = (bmp_txt_grp_rect.top + (bmp_txt_grp_rect.height() / 2f + getTextBoundHeightWithBottom(center_string, textPaint)/2f)); //draw from baseline
-            right_txt_rect = new Rect((int) left_bmp_rect.right, left_bmp_rect.top, bmp_txt_grp_rect.right, (int) yTxt_2);
-            // canvas.drawRect(right_txt_rect, imgShapePaint);//black bg
-            canvas.drawText(center_string, left_bmp_rect.right, yTxt_2, textPaint);
+                //left bmp
+                //config left bmp
+                float left_area_width = bmp_txt_grp_rect.width() * 0.5f;
+                left_bmp = Bitmap.createScaledBitmap(left_bmp, (int) left_area_width, (int) left_area_width, false);
+                float middle_y = bmp_txt_grp_rect.centerY() - left_bmp.getHeight()/2f; //draw from top - left
+                left_bmp_rect = new Rect((int) (bmp_txt_grp_rect.centerX() - left_area_width), (int) middle_y, bmp_txt_grp_rect.centerX(), (int) (middle_y + left_bmp.getHeight()));
+                // canvas.drawRect(left_bmp_rect, img_shape_paint);//black bg
+                canvas.drawBitmap(left_bmp, new Rect(0, 0, left_bmp.getWidth(), left_bmp.getHeight()), left_bmp_rect, img_shape_paint);
 
-        // float textWidthFactor = getTextBoundWidthOfString(center_string, textPaint) / (float) bitmap_txt_group_rect.width();
-        // float bmpWidthFactor = (1 - textWidthFactor);
 
+                //right text
+                //config right text
+                if(isFirst) {
+                    isFirst = false;
+                    float right_area_width = bmp_txt_grp_rect.width() * 0.5f;
+                    yTxt_2 = (bmp_txt_grp_rect.centerY() + getTextBoundHeightWithBottom(center_string, right_text_paint) / 2f); //draw from baseline
+                    right_txt_rect = new Rect(bmp_txt_grp_rect.centerX(), left_bmp_rect.top, bmp_txt_grp_rect.centerX() + (int) right_area_width, (int) yTxt_2);
+                    float txtSize = DrawKnobActivity.getFitTextSizeHorizontal(right_text_paint, right_area_width, center_string); //calculate text font size
+                    right_text_paint.setTextSize(txtSize); //calculate text font size
+                }
+                canvas.drawRect(right_txt_rect, img_shape_paint);//black bg
+                canvas.drawText("x " + center_string, bmp_txt_grp_rect.centerX(), yTxt_2, right_text_paint);
+        }
     }
 
     private Point downPt = new Point(); //must be global
@@ -192,7 +257,7 @@ public class MyKnob extends View implements View.OnTouchListener {
                 mActivePointerID = event.getPointerId(0);
                 downPt.x = (int) event.getX();
                 downPt.y = (int) event.getY();
-                boolean is_inside = testRect.contains(downPt.x, downPt.y);
+                boolean is_inside = rect_outer.contains(downPt.x, downPt.y);
                 if (is_inside) {
                     stat = EVENT_STAT.MOVE;
                     LogI("trong HCN");
