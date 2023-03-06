f750df2a12ac24a234476b3cbce16e489118cf6d
dotrinh <contact@dotrinh.com>
2023-03-06 20:44:29 +0900

    crop circle bmp

---
+++ app/src/main/java/com/dotrinh/canvas/draw_bitmap/BMP_MainActivity.java
@@ -0,0 +1,70 @@
+/*
+ * Created by dotrinh on 7/22/20 8:04 PM
+ * Copyright (c) 2020. dotr Inc. All rights reserved.
+ */
+
+package com.dotrinh.canvas.draw_bitmap;
+
+import static com.dotrinh.protool.LogUtil.LogI;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dotrinh.canvas.R;
+import com.dotrinh.canvas.draw_LED.MyClipPath_LED;
+
+public class BMP_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bitmap);
+        ImageView img = findViewById(R.id.imageView);
+        Bitmap res = BitmapFactory.decodeResource(getResources(), R.drawable.nongthonvn);
+        img.setImageBitmap(getCircularBitmap(res));
+    }
+
+    public static Bitmap getCircularBitmap(Bitmap bitmap) {
+        Bitmap output;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            output = Bitmap.createBitmap(bitmap.getHeight(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        } else {
+            output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getWidth(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        float r = 0;
+
+        if (bitmap.getWidth() > bitmap.getHeight()) {
+            r = bitmap.getHeight() / 2f;
+        } else {
+            r = bitmap.getWidth() / 2f;
+        }
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(r, r, r, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+}
